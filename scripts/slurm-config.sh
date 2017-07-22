62b9bb702593a0dbc6fb84d1c3626ff49a12664e
James W. McClain <jmcclain@daystrom-data-concepts.com>
2017-07-21 23:04:10 -0400

    Configure All Nodes

--- scripts/slurm-config.sh
+++ scripts/slurm-config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-LEADER=${1:-"leader"}
-NODES=${2:-"leader"}
+LEADER=${1:-$HOSTNAME}
+NODES=${2:-$HOSTNAME}
 CPUS=${3:-"ThreadsPerCore=2 CoresPerSocket=8 Sockets=2"}
 
 sed -e "s/XXX/$LEADER/" -e "s/YYY/$NODES/" -e "s/ZZZ/$CPUS/" \
